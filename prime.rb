700683233e17aff352e1e7156bca05d5d29e7899
Rocio Batres <jonasgirl21@gmail.com>
2020-09-15 20:56:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,9 @@
 def prime?(num)
+  (2..(num - 1)).each do |interger|
+  return false if num % interger == 0   
+  end
+  true
   if num < 0 
     false 
-  else   
-    (2..(num - 1)).each do |interger|
-    return false if num % interger == 0   
-    end 
   end   
-  true
-  
 end 
